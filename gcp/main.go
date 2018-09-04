b4623c3566eff18dfb6e242dc188b28554a3cbe7
bluemon0919 <bluemon0919@yahoo.co.jp>
2018-09-04 17:25:55 +0900

    test09042

--- gcp/main.go
+++ gcp/main.go
@@ -1,21 +1,6 @@
-// Copyright 2016 LINE Corporation
-//
-// LINE Corporation licenses this file to you under the Apache License,
-// version 2.0 (the "License"); you may not use this file except in compliance
-// with the License. You may obtain a copy of the License at:
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations
-// under the License.
-
-package main
+package init
 
 import (
-	"log"
 	"net/http"
 	"os"
 
@@ -25,21 +10,19 @@ import (
 	"google.golang.org/appengine/urlfetch"
 )
 
+
 func init() {
 	// Setup HTTP Server for receiving requests from LINE platform.
 	http.HandleFunc("/callback", handler)
-
-
-	http.ListenAndServe(":8080", nil)
-//	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 
 	bot, err := linebot.New(
-		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
-		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+		os.Getenv("1a5df2f9ef2381b290c3954b1ff0dd3f"),
+		os.Getenv("WRyh+ZsToMynMG5EoCJR8StGTCdLi6y78WeEIxfZo7G1MQiIOuihTaZbf47BDFsy34zd7gtHht2aXiCPuL4Ub07jlom36MMTDP9NIusH7dgtDPzlwX0xNwOH0SYmMmrxrOGMPYd6rRI65kljAy8+xQdB04t89/1O/w1cDnyilFU="),
 		linebot.WithHTTPClient(urlfetch.Client(ctx)),
 	)
 	if err != nil {
@@ -62,7 +45,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-					log.Print(err)
+					log.Errorf(ctx, "LINE bot reply message error.: %v", err)
 				}
 			}
 		}
